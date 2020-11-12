e6a669c6eaaf8f06b2798fb685c8e4f55dc02072
Aryan Singla <58567476+AryanS62@users.noreply.github.com>
2020-11-12 21:20:24 +0530

    Add files via upload

---
+++ Assignment_5/q1.java
@@ -0,0 +1,21 @@
+public class q1 extends Thread
+	{
+	    public static void main(String[] args)
+	    {
+	        ques1 obj = new ques1();
+	        obj.start();
+	    }
+
+	    public void run()
+	    {
+	        for(int i=1;i<=100;i++)
+	        {
+	            System.out.println(i);
+	            try
+	            {
+	                Thread.sleep(1000);
+	            }catch(Exception e){}
+	        }
+	    }
+
+	}
